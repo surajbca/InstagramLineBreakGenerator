30ac76111d47a770fb3902a5cd6be6c850cf406f
surajbca <krsurajkumar7481@gmail.com>
2023-05-25 19:23:48 +0530

    Add instagram line breack generator

--- InstagramLineBreakGenerator/clear.js
+++ InstagramLineBreakGenerator/clear.js
@@ -2,4 +2,16 @@ function ClearValue() {
   document.getElementById("copyValue").value = "";
 
   document.getElementById("stringLength").innerHTML = "0";
+
+  let bold = document.getElementById("copyValue");
+  bold.style.fontWeight = "";
+
+  let italic = document.getElementById("copyValue");
+  italic.style.fontStyle = "";
+
+  let cursive = document.getElementById("copyValue");
+  cursive.style.fontFamily = "";
+
+  let fantasy = document.getElementById("copyValue");
+  fantasy.style.fontFamily = "";
 }
---
+++ InstagramLineBreakGenerator/fontfamily.js
@@ -0,0 +1,19 @@
+function bold() {
+  let bold = document.getElementById("copyValue");
+  bold.style.fontWeight = "bold";
+}
+
+function italic() {
+  let italic = document.getElementById("copyValue");
+  italic.style.fontStyle = "italic";
+}
+
+function cursive() {
+  let cursive = document.getElementById("copyValue");
+  cursive.style.fontFamily = "cursive";
+}
+
+function fantasy() {
+  let fantasy = document.getElementById("copyValue");
+  fantasy.style.fontFamily = "fantasy";
+}

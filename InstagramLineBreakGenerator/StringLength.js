e0987520ac1b6c9e639e93fc36432c6e2530173a
surajbca <krsurajkumar7481@gmail.com>
2023-05-29 19:34:59 +0530

    setup

--- InstagramLineBreakGenerator/StringLength.js
+++ InstagramLineBreakGenerator/StringLength.js
@@ -2,8 +2,4 @@ function length() {
   var txt = document.getElementById("copyValue").value;
   var len = txt.length;
   document.getElementById("stringLength").innerHTML = len;
-
-  /*if (len > 0) {
-    document.write("Sucess"); // if len > 0 do some action
-  } else document.write(txt); //else condition*/
 }
--- InstagramLineBreakGenerator/copy.js
+++ InstagramLineBreakGenerator/copy.js
@@ -1,15 +1,22 @@
 function copyValue() {
-  const copyValue = document.getElementById("copyValue");
+  let copyValue = document.getElementById("copyValue");
 
-  //copyValue.select();
-  // copyValue.setSelectionRange(0, 99999); // For mobile devices
+  let chunks = [];
+  const chunkSize = 62;
+
+  let newValue = copyValue.value;
+  //alert(newValue.length);
+  // navigator.clipboard.writeText(chunks.join("\n"));
+
+  while (newValue.length > 0) {
+    chunks.push(newValue.substring(0, chunkSize));
+    newValue = newValue.substring(chunkSize, newValue.length);
+  }
+  // alert(chunks.join("\n"));
+  brokenValue = chunks.join("\n");
+  copyValue.select();
+  copyValue.setSelectionRange(0, 99999); // For mobile devices
 
   // Copy the text inside the text field
-  navigator.clipboard.writeText(copyValue.value);
-  //alert("Copied the text: " + copyValue.value);
-
-  //var l = document.getElementById("textarea1").value;
-  if (copyValue.length > 10) {
-    copyValue = copyValue + "<br />";
-  }
+  navigator.clipboard.writeText(brokenValue);
 }
